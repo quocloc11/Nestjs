fdbbbd987ba4e55cbfaed1068a805d458803b12b
unknown <loc842211@gmail.com>
2024-04-11 09:39:09 +0700

    fig ENV

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()//() bao hieu  dug ham nay
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService,
+    private readonly configService: ConfigService
+  ) { }
 
   @Get()
   @Render("home")
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://quocloc:quocloc@cluster0.yjmdk8u.mongodb.net/quocloc')],
+  imports: [
+    // [MongooseModule.forRoot(''),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (ConfigService: ConfigService) => ({
+        uri: ConfigService.get<string>('MONGO_URL')
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
