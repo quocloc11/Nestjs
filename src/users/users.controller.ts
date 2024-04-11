58634fd8e58350d8f44299cb5b32e4d9bedbf238
unknown <loc842211@gmail.com>
2024-04-11 12:18:13 +0700

    dato

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,14 +8,15 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
-  create
-    (@Body("email") email: string,
-      @Body("password") password: string,
-      @Body("name") name: string,
-    ) { //CreateUserDto kieu gia tri
+  create(
+    // (@Body("email") email: string,
+    //   @Body("password") password: string,
+    //   @Body("name") name: string,
+    @Body() quocloc: CreateUserDto
+  ) { //CreateUserDto kieu gia tri
     //req.body
     //= const myEmail: string =req.body.email//string
-    return this.usersService.create(email, password, name);
+    return this.usersService.create(quocloc);
   }
 
   @Get()
