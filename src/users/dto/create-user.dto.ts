8df5eb893a52b26ba47170bb6b6f8cf0cf9cc04b
unknown <loc842211@gmail.com>
2024-04-11 12:36:03 +0700

    validate pipe

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsEmail({},
+    {
+      message: 'Email dung dinh dang'
+    })
+  @IsNotEmpty({
+    message: 'Email ko dc de trong'
+  })
   email: string;
+  @IsNotEmpty({
+    message: 'Password ko dc de trong'
+  })
   password: string;
   name: string;
   address: string;
